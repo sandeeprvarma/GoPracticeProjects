59a7f35cef3cefac06bc0015e4f7b3b81d29e897
sandeeprvarma <10071454+sandeeprvarma@users.noreply.github.com>
2025-06-25 08:10:42 +0530

    Adding Pratice code

--- bank/bank.go
+++ bank/bank.go
@@ -2,20 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	var accountBalance = 1000.0
-	var choice int
-	var deposite, withdraw float64
+var accountBalance = 1000.0
 
+func main() {
+
+	for i := 0; i < 2; i++ {
+		choice := welcomeUser()
+		bankChoiceActions(choice)
+	}
+	// fmt.Println("Final Balance:", accountBalance)
+}
+
+func welcomeUser() (choice int) {
 	fmt.Println("Welcome to go bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposite Money")
 	fmt.Println("3. Withdraw Money")
 	fmt.Println("4. Exit")
-
 	fmt.Scan(&choice)
+	return choice
+}
 
+func bankChoiceActions(choice int) {
+	var deposite, withdraw float64
 	// if else if
 	if choice == 1 {
 		fmt.Println("Your account balance is: ", accountBalance)
